64c1c4868ff91a1ae7f9da151c3966a78ce62fde
krhickssmu <167340617+krhickssmu@users.noreply.github.com>
2024-04-22 11:29:36 -0500

    Update ItemToPurchase.cpp

--- ItemToPurchase.cpp
+++ ItemToPurchase.cpp
@@ -1,1 +1,57 @@
+#include <iostream>
+using namespace std;
 
+#include "ItemToPurchase.h"
+
+//constructors
+ItemToPurchase::ItemToPurchase(){
+  itemName="none";
+  itemDescription="none";
+  itemPrice=0;
+  itemQuantity=0;
+}
+ItemToPurchase::ItemToPurchase(string name, string description, int price, int quantity = 0){
+  itemName=name;
+  itemDescription=description;
+  itemPrice=price;
+  itemQuantity=quantity;
+}
+
+//setters
+void SetName(string name){
+  itemName = name;
+}
+void SetDescription(string description){
+  itemDescription = description;
+}
+void SetPrice(int price){
+  itemPrice = price;
+}
+void SetQuantity(int quantity){
+  itemQuantity = quantity;
+}
+
+//getters
+string GetName() const{
+  return itemName;
+}
+string GetDescription() const{
+  return itemDescription;
+}
+int GetPrice() const{
+  return itemPrice;
+}
+int GetQuantity() const{
+  return itemQuantity;
+}
+
+//printers
+void PrintItemCost(){
+  cout<<itemName<<" "<<itemQuantity<<" @ $"<<cout<<itemPrice<<" = "<<itemQuantity*itemPrice<<endl;
+}
+void PrintItemDescription(){
+  cout<<itemName<<": "<<itemDescription<<endl;
+}
+  
+   
+  
