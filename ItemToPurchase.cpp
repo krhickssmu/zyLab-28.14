c55533af01f181280d557aa21baf58c87aeb1077
RuthA120 <167146819+RuthA120@users.noreply.github.com>
2024-04-24 11:35:53 -0500

    Updated ItemToPurchase.cpp

--- ItemToPurchase.cpp
+++ ItemToPurchase.cpp
@@ -1,1 +1,54 @@
+#include <iostream>
+using namespace std;
 
+#include "ItemToPurchase.h"
+
+//constructors
+ItemToPurchase::ItemToPurchase(){
+  itemName="none";
+  itemDescription="none";
+  itemPrice=0;
+  itemQuantity=0;
+}
+ItemToPurchase::ItemToPurchase(string name, string description, int price, int quantity){
+  itemName=name;
+  itemDescription=description;
+  itemPrice=price;
+  itemQuantity=0;
+}
+
+//setters
+void ItemToPurchase::SetName(string name){
+  this->itemName = name;
+}
+void ItemToPurchase::SetDescription(string description){
+  this->itemDescription = description;
+}
+void ItemToPurchase::SetPrice(int price){
+  this->itemPrice = price;
+}
+void ItemToPurchase::SetQuantity(int quantity){
+  this->itemQuantity = quantity;
+}
+
+//getters
+string ItemToPurchase::GetName() const{
+  return itemName;
+}
+string ItemToPurchase::GetDescription() const{
+  return itemDescription;
+}
+int ItemToPurchase::GetPrice() const{
+  return itemPrice;
+}
+int ItemToPurchase::GetQuantity() const{
+  return itemQuantity;
+}
+
+//printers
+void ItemToPurchase::PrintItemCost(){
+  cout<<itemName<<" "<<itemQuantity<<" @ $"<<itemPrice<<" = "<<itemQuantity*itemPrice<<endl;
+}
+void ItemToPurchase::PrintItemDescription(){
+  cout<<itemName<<": "<<itemDescription<<endl;
+}
